77eb98cd54ae6862fc59a06477e1048cfd00c5b4
jongbokwvn <jbw3016@hanyang.ac.kr>
2025-04-30 08:12:40 +0000

    Deploying to gh-pages from @ jongbokwvn/jongbokwvn.github.io@b33c5ce547d80fdcb06bc7e15d3f60ee69cca0ff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "post-",
+        
+          title: "",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/2025-04-30-Complexodynamics/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
